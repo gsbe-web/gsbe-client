2392d2f305ebd07276cb5747fc122e0c080aab9e
Prince Gbezeh <gbezehp@gmail.com>
2025-06-18 18:28:08 +0000

    feat: add backdrop to about-hero

--- src/components/About/Hero.tsx
+++ src/components/About/Hero.tsx
@@ -2,12 +2,14 @@ import { Link } from "react-router";
 
 export const AboutHero = () => {
 	return (
-		<div className="w-dvw h-[55dvh]">
-			<img
-				src="/images/officialgsbe_cover_about.jpeg"
-				alt="hero"
-				className="object-cover w-full h-full"
-			/>
+		<div>
+			<div className="w-dvw h-[55dvh] relative after:content-[''] after:absolute after:inset-0  after:bg-black/50">
+				<img
+					src="/images/officialgsbe_cover_about.jpeg"
+					alt="hero"
+					className="object-cover w-full h-full"
+				/>
+			</div>
 
 			<div>
 				<span className="text-[#FFFFFF]  absolute left-1/8 lg:left-1/5 top-1/7  lg:top-2/7 ">
