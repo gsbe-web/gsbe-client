1d7a486bbab046fc8189281235f2dd837abee3e5
Prince Gbezeh <gbezehp@gmail.com>
2025-06-21 11:29:54 +0000

    feat: add executives page

--- src/pages/executives/index.tsx
+++ src/pages/executives/index.tsx
@@ -2,7 +2,7 @@ import { Team } from "@components/Executives";
 
 export function OurExecutives() {
 	return (
-		<div className="flex min-h-screen w-full flex-col gap-4 bg-[#EAEAE8]">
+		<div className="flex min-h-screen w-full flex-col gap-4 bg-white">
 			<div>
 				<Team />
 			</div>
