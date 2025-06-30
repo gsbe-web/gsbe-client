030062c8da540953d81068dbd4be45fa3855c12e
Prince Gbezeh <gbezehp@gmail.com>
2025-06-30 09:28:48 +0000

    fix: view full list button display

--- src/components/Membership/Message.tsx
+++ src/components/Membership/Message.tsx
@@ -10,14 +10,14 @@ export const Message = () => {
 				<h2 className=" uppercase font-extrabold md:text-3xl tracking-[5px] ">
 					Dedication. Expertise. Passion.
 				</h2>
-				<p className="py-10 pb-18 lg:px-80 font-semibold md:text-2xl tracking-wide  ">
+				<p className="py-10 md:pb-18 lg:px-80 font-semibold md:text-2xl tracking-wide  ">
 					GSBE recognizes members who consistently demonstrate our values and
 					contribute positively to our community. We commend their dedication
 					and acknowledge their significant role in fostering a supportive
 					environment. Thank you for being an integral part of our journey. We
 					say Ayekoo.
 				</p>
-				<Button className="rounded-none bg-[#282626] hover:bg-white hover:text-[#254152] tracking-wide text-lg font-light p-6">
+				<Button className="rounded-none bg-[#282626] hover:bg-white hover:text-[#254152] tracking-wide md:text-lg font-light md:p-6">
 					View Full List
 				</Button>
 			</div>
