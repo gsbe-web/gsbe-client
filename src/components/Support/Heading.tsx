b6825d788be4fa39153924b9ef16834c740e2b01
Prince Gbezeh <gbezehp@gmail.com>
2025-07-01 15:48:35 +0000

    feat: add dues info

---
+++ src/components/Support/Heading.tsx
@@ -0,0 +1,12 @@
+export const Heading = () => {
+	return (
+		<div className="text-center py-8">
+			<h1 className="uppercase text-[#D55342] tracking widest text-5xl">
+				pay your membership dues
+			</h1>
+			<small className="font-semibold text-lg">
+				Ensure your membership remains active by paying your dues on time.
+			</small>
+		</div>
+	);
+};
